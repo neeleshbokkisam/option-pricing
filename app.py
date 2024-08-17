6712c5b4173fe608f89dafc122e83e5060ff4ee6
neeleshbokkisam <saineelesh23@gmail.com>
2024-08-17 02:24:32 -0700

    railyway sutff

--- app.py
+++ app.py
@@ -125,5 +125,6 @@ def binomial_tree():
     return render_template('binomial_tree_form.html', default_exercise_date=default_exercise_date, datetime=datetime, timedelta=timedelta)
 
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+
